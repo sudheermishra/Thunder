ad216de4db3f2424a313e0861d3871c563280c39
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-07-06 12:34:28 +0530

    add: project on github api

---
+++ javascript/project11/index.js
@@ -0,0 +1,43 @@
+async function gitHub(number = 20) {
+  const element = document.getElementById("root");
+  // taaki yeh function call ho jab phle yeh empty after that jo bhi value pass hui function me woh chali jaye
+  element.textContent = "";
+
+  const response = await fetch(
+    `https://api.github.com/users?per_page=${number}`,
+  );
+  const data = await response.json();
+  console.log(data);
+
+  for (const user of data) {
+    //  image
+    let img = document.createElement("img");
+    img.src = user.avatar_url;
+
+    // name
+    let name = document.createElement("h2");
+    name.textContent = user.login;
+
+    // card
+    let container = document.createElement("div");
+    container.append(img, name);
+    // container.append(name);
+
+    element.append(container);
+  }
+}
+
+gitHub();
+
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+
+btn.addEventListener("click", () => {
+  const number = Number(input.value);
+  gitHub(number);
+});
+
+input.addEventListener("input", () => {
+  const number = Number(input.value);
+  gitHub(number);
+});
